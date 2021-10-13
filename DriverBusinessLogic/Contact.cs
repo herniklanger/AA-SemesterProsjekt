bbc8b7c1b86530d2fef13dc6c84e53666fd889c2
OguzDastan <33421040+OguzDastan@users.noreply.github.com>
2021-10-13 11:25:35 +0200

    driver
    driver interfaces and classes implemented. DriverController added

---
+++ DriverBusinessLogic/Contact.cs
@@ -0,0 +1,17 @@
+﻿using InterfacesLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverBusinessLogic
+{
+    public class Contact : IContact<Driver>
+    {
+        public int Id { get; set; }
+        public Driver DriverId { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Type { get; set; }
+    }
+}
