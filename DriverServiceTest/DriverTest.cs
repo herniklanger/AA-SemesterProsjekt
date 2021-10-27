caddde835b1806e3fbef56c4c6f0100339b1b70e
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2021-10-27 11:49:19 +0200

    Addet unitests

---
+++ DriverServiceTest/DriverTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace DriverServiceTest
+{
+    public class DriverTest
+    {
+       
+        [Fact]
+        public void GetDriverTest()
+        {
+            //Arrange
+            var driver = new GetCurrentDriver();
+
+            //Act
+            var result = driver.IsFound();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
