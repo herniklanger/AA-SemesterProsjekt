8b665f97c57f7c11ff2e914782dfc70d8336dfc8
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2021-10-29 23:12:54 +0200

    Addet loginPage

---
+++ Frontend/DriverApp/Pages/Nvigation.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DriverApp.Pages
+{
+    public class Nvigation : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
