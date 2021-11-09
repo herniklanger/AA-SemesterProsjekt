be2cf2c0be00e63558efd9ede062559b2f91beb3
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2021-11-09 10:34:20 +0100

    addet Google maps på frondend

--- Frontend/DriverApp/Pages/Navigation.razor.cs
+++ Frontend/DriverApp/Pages/Navigation.razor.cs
@@ -1,7 +1,21 @@
-﻿namespace DriverApp.Pages
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DriverApp.Pages
 {
     public partial class Navigation
     {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
         
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("initialize", null); 
+            }
+        }
     }
+    
 }
