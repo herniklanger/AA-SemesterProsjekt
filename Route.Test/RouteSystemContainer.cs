6612875dcbb6860141c3f954ee1231a638577057
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2021-11-11 10:45:38 +0100

    Replase db Route testende men en Inmemmroy db.

--- Route.Test/RouteSystemContainer.cs
+++ Route.Test/RouteSystemContainer.cs
@@ -1,5 +1,11 @@
-﻿using System.Net.Http;
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
 
 namespace Route.Test
 {
@@ -15,15 +21,15 @@ namespace Route.Test
                 {
                     builder.ConfigureServices(services =>
                     {
-                        //services.RemoveAll(typeof(IDbConnection));
-                        //services.RemoveAll(typeof(IDbConnectionFactory));
-                        //services.AddSingleton<IDbConnection>(options => new SQLiteConnection(":memory:"));
-                        //services.AddSingleton<IDbConnectionFactory>(options =>
-                        //{
-                        //    IDbConnection connection = options.GetService<IDbConnection>();
-                        //    OrmLiteConnectionFactory connectionFactory = new(connection.ConnectionString, SqliteDialect.Provider);
-                        //    return connectionFactory;
-                        //});
+                        services.RemoveAll(typeof(IDbConnection));
+                        services.RemoveAll(typeof(IDbConnectionFactory));
+                        services.AddSingleton<IDbConnection>(options => new SQLiteConnection(":memory:"));
+                        services.AddSingleton<IDbConnectionFactory>(options =>
+                        {
+                            IDbConnection connection = options.GetService<IDbConnection>();
+                            OrmLiteConnectionFactory connectionFactory = new(connection.ConnectionString, SqliteDialect.Provider);
+                            return connectionFactory;
+                        });
                     });
                 });
             TestClient = app.CreateClient();
